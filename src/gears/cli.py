a89d5e0e0965ac7072eec3f6cccd48ef4b969bfc
Kazuya Takei <myself@attakei.net>
2023-12-22 01:32:32 +0900

    feat: Stub command

--- src/gears/cli.py
+++ src/gears/cli.py
@@ -1,9 +1,20 @@
+"""CLI entrypoint."""
 import click
 
+CONTEXT_SETTINGS = {
+    "help_option_names": ["-h", "--help"],
+}
 
-@click.group()
-def app():
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def app():  # noqa: D103
     pass
+
+
+@app.command()
+def init():
+    """Initialize workspace."""
+    click.echo(click.style("This is not implemented!!", fg="red"))
 
 
 def main():
